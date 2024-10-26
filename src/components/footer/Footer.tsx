e92f6d38b7c3d2f5e6b0027f65fcb4a9648597f2
giorgi <zautashvili1994@gmail.com>
2024-10-26 16:01:34 +0400

    change icons src

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,14 +7,14 @@ const Footer: React.FC = () => {
         <a href="https://www.facebook.com/zautagio" target="_blank">
           <img
             className={styles.icon}
-            src="../../../public/icons/facebook.png"
+            src="/public/icons/facebook.png"
             alt="facebook"
           />
         </a>
         <a href="https://github.com/giozauta" target="_blank">
           <img
             className={styles.icon}
-            src="../../../public/icons/github.png"
+            src="/public/icons/github.png"
             alt="github"
           />
         </a>
@@ -24,7 +24,7 @@ const Footer: React.FC = () => {
         >
           <img
             className={styles.icon}
-            src="../../../public/icons/linkedin.png"
+            src="/public/icons/linkedin.png"
             alt="linkedin"
           />
         </a>
