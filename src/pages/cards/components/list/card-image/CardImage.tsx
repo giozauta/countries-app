2f224fc6506d24e8ad6f8369af76f3321adc07c6
giorgi <zautashvili1994@gmail.com>
2024-10-28 01:13:43 +0400

    add new css to cards

--- src/pages/cards/components/list/card-image/CardImage.tsx
+++ src/pages/cards/components/list/card-image/CardImage.tsx
@@ -1,9 +1,19 @@
 import styles from "./cardImage.module.css";
+import { useState } from "react";
 
 const CardImage: React.FC<{ imgSrc: string }> = ({ imgSrc }) => {
+  const [isContentVisible, setIsContentVisible] = useState(false);
+
+  const handleShowContent = () => {
+    setIsContentVisible((prev) => !prev); // Toggle visibility
+  }
+  
   return (
-    <div className={styles.cardImageBox}>
-      <img alt="card" className={styles.cardImage} src={imgSrc} />
+    <div 
+      className={styles.cardImageBox}
+    >
+      <img  alt="card" className={isContentVisible?styles.cardImage:styles.cardImageOver} src={imgSrc} />
+      <div onClick={handleShowContent} className={styles.clickMe}>Click Me</div>
     </div>
   );
 };
