2326b25e6d03e0bce6808f0c6a766644da538080
giorgi <zautashvili1994@gmail.com>
2024-11-09 13:12:09 +0400

    add api to base url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 const axiosConfig: AxiosRequestConfig = {
-  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
 };
 
 export const httpClient = axios.create(axiosConfig);
